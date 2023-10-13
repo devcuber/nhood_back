8b5a8b395e86e92bb4af409dc3e1ac2c715597fa
Jose Jimenez <luis.jimenez@weirdneutrino.io>
2023-10-12 18:01:12 -0600

    allowed host pythonanywhere

--- nhood_back/settings.py
+++ nhood_back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yi$=tq2y&gf-xt)dyzoazy%_#z&$n%&63=d#-hr-ikbk$cjcl!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','192.168.0.105']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','192.168.0.105','devcuber.pythonanywhere.com']
 
 
 # Application definition
